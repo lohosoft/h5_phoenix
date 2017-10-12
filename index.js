f1d26934f8a58c30a753b673c5522f2a05c01606
niexiaoguang <nxg@lohosft.com>
2017-10-12 16:23:00 +0800

    total handle do dm manually

--- index.js
+++ index.js
@@ -1,46 +1,75 @@
 console.log("index.js");
-const myGlobal = {};
-myGlobal.songfile = "assets/zxmzf.mp3";
+var myGlobal = {};
+
 window.onload = init;
 function init() {
+	if (!window.DeviceOrientationEvent && !window.DeviceMotion) {
+		noluck();
+	} else {
+		prepare();
+	}
+}
+
+function prepare() {
+	myGlobal.ready = false;
+	// prepare init works
+	// then wait start button push
+	myGlobal.songfile = "assets/zxmzf.mp3";
+
 	myGlobal.song = new buzz.sound(myGlobal.songfile);
 
-	myGlobal.gn = new GyroNorm();
+	myGlobal.doOld = [0, 0, 0];
+	myGlobal.doNew = [0, 0, 0];
+	myGlobal.dmOld = [0, 0, 0];
+	myGlobal.dmNew = [0, 0, 0];
+	window.addEventListener(
+		"deviceorientation",
+		function(event) {
+			var alpha = event.alpha;
+			var beta = event.beta;
+			var gamma = event.gamma;
+			updateDO(alpha, beta, gamma);
+		},
+		false
+	);
+	window.addEventListener("devicemotion", function(event) {
+		var x = event.acceleration.x;
+		var y = event.acceleration.y;
+		var z = event.acceleration.z;
 
-	myGlobal.gn
-		.init()
-		.then(function() {
-			gn.start(function(data) {
-				// console.log(JSON.stringify(data));
-				// infoDiv1.innerHTML = "<p>" + JSON.stringify(data) + "</p>";
-				// Process:
-				// data.do.alpha	( deviceorientation event alpha value )
-				// data.do.beta		( deviceorientation event beta value )
-				// data.do.gamma	( deviceorientation event gamma value )
-				// data.do.absolute	( deviceorientation event absolute value )
-				// data.dm.x		( devicemotion event acceleration x value )
-				// data.dm.y		( devicemotion event acceleration y value )
-				// data.dm.z		( devicemotion event acceleration z value )
-				// data.dm.gx		( devicemotion event accelerationIncludingGravity x value )
-				// data.dm.gy		( devicemotion event accelerationIncludingGravity y value )
-				// data.dm.gz		( devicemotion event accelerationIncludingGravity z value )
-				// data.dm.alpha	( devicemotion event rotationRate alpha value )
-				// data.dm.beta		( devicemotion event rotationRate beta value )
-				// data.dm.gamma	( devicemotion event rotationRate gamma value )
-			});
-		})
-		.catch(function(e) {
-			// Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
-			// alert('not support your device yet')
-			window.addEventListener("deviceorientation", function(e) {
-				// infoDiv1.innerHTML = "<p>" + JSON.stringify(e) + "</p>";
-			});
-			window.addEventListener("devicemotion", function(e) {
-				// infoDiv2.innerHTML = "<p>" + JSON.stringify(e) + "</p>";
-			});
-		});
-	function start() {
-		song.play();
-		song.loop();
+		// var ralpha = event.rotationRate.alpha;
+		// var rbeta = event.rotationRate.beta;
+		// var rgamma = event.rotationRate.gamma;
+
+		// var interval = event.interval;
+		updateDM(x, y, z);
+	});
+	myGlobal.ready = true;
+}
+function start() {
+	if (myGlobal.ready) {
+		tick();
+	} else {
+		console.log("not ready yet");
+		alert("not ready yet");
 	}
+}
+
+function tick() {}
+function updateDO(alpha, beta, gamma) {
+	myGlobal.doOld = myGlobal.doNew;
+	myGlobal.doNew[0] = alpha;
+	myGlobal.doNew[1] = beta;
+	myGlobal.doNew[2] = gamma;
+}
+
+function updateDM(x, y, z) {
+	myGlobal.dmOld = myGlobal.dmNew;
+	myGlobal.dmNew[0] = x;
+	myGlobal.dmNew[1] = y;
+	myGlobal.dmNew[2] = z;
+}
+
+function noluck() {
+	alert("noluck");
 }
